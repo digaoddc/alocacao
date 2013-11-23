e74ae7efceea2fd1ad5a4c863a50b243b6a1695b
Rodrigo <digao@osfeio.com>
2013-11-23 17:36:13 -0200

    alocacao professor controller

---
+++ alocacao/alocacao/Controllers/AlocacaoProfessorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alocacao.Controllers
+{
+    public class AlocacaoProfessorController : Controller
+    {
+        //
+        // GET: /AlocacaoProfessor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
