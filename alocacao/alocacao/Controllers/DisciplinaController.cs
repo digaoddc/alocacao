c30fec8ed5a3fb7862fcec6c3784d6a2cae5067a
midorikjo <midori.kinjo@gmail.com>
2013-11-24 16:41:17 -0200

    Disciplina, porém ainda não está gravando no XML

---
+++ alocacao/alocacao/Controllers/DisciplinaController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace alocacao.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        //
+        // GET: /Disciplina/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Disciplina/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Disciplina/Create
+
+        public ActionResult New()
+        {
+            return View();
+        } 
+
+        //
+                
+        public bool Exportar(String caminho)
+        {
+            try
+            {
+                Stream stream = new FileStream(caminho, FileMode.Create);
+                XmlSerializer serializador = new XmlSerializer(typeof(DisciplinaController));
+                serializador.Serialize(stream, this);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        //
+        // GET: /Disciplina/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Disciplina/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Disciplina/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Disciplina/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ alocacao/alocacao/Views/Disciplina/New.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	New
+     <script language="javascript" type="text/javascript">
+// <![CDATA[
+
+         function gravarDisc_onclick() {
+             create()
+         }
+
+// ]]>
+    </script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Cadastro de Disciplina</h2>
+  
+       <form id="form1" runat="server" class="form-horizontal">
+    <div class="form-group">
+    
+    <asp:Label ID="Lblnome" runat="server" Text="Nome da disciplina: " class="control-label col-sm-3 col-md-2"></asp:Label>
+    <asp:TextBox ID="Txtnome" runat="server" Width="359px" class="form-control"></asp:TextBox>
+    
+        <br />
+    
+    <asp:Label ID="Lblsigla" runat="server" Text="Sigla: " class="control-label col-sm-3 col-md-2"></asp:Label>
+    <asp:TextBox ID="Txtsigla" runat="server" Width="133px" class="form-control" ></asp:TextBox>
+    
+        <br />
+    
+    <asp:Label ID="Lblcarga" runat="server" Text="Carga horária: " class="control-label col-sm-3 col-md-2"></asp:Label>
+    <asp:TextBox ID="Txtcarga" runat="server" class="form-control" Width="133px" ></asp:TextBox>
+           
+    
+        <br />
+        <br />
+        <input id="gravarDisc" type="submit" value="Salvar" onclick="return gravarDisc_onclick()" /></div>
+    </form>
+
+</asp:Content>
